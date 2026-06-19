af364a9bb310363cd11b83580ea45823a3812c6a
dbaschi <165275693+dbaschi@users.noreply.github.com>
2026-06-20 00:34:44 +0200

    Update main.js

--- main.js
+++ main.js
@@ -36,153 +36,163 @@ const categories = cat.flatMap(str => str.slice(1).replaceAll(" ", "+"));
 const sus = Object.values(data);
 const words = sus.flatMap(str => str.split(", "));
 
-console.log(cat[2]);
-console.log(words);
-
 
 const grid = document.getElementById("grid");
 var selections=0;
 var max=0;
 var solutions=0;
 var lifes =4;
-function Solve(matchedButtons,cleanClass,categories, document, solutions, colorArray, cat){
-    var text=[];
-    const index = categories.indexOf(cleanClass);
-    console.log("qui");
-    console.log(cat);
-    let sos= categories.flatMap(str => str.replaceAll("+", " "));
-     // Mark them as solved
-    matchedButtons.forEach(btn => {
-        btn.classList.remove("active");
+function Solve(matchedButtons,cleanClass,categories, document, solutions, colorArray, cat, numbers){
+  var text=[];
+  const index = categories.indexOf(cleanClass);
+  console.log("qui");
+  console.log(cat);
+  let sos= categories.flatMap(str => str.replaceAll("+", " "));
 
-        text=text + btn.textContent+', ';
-        if (solutions===0){
-          btn.classList.add("first")
-          document.getElementById("one").style.opacity = "1.0";
-          document.getElementById("one").style.backgroundColor = colorArray[index];
-          document.getElementById("one").innerHTML = sos[index]+ "<br>"+ text;
-          console.log("punto");
-        }
-        if (solutions===1){
-          btn.classList.add("second") 
-          document.getElementById("two").style.opacity = "1.0";
-          document.getElementById("two").style.backgroundColor = colorArray[index];
-          document.getElementById("two").innerHTML = sos[index]+ "<br>"+ text;
-        }
-        if (solutions===2){
-          btn.classList.add("third") 
-          document.getElementById("three").style.opacity = "1.0";
-          document.getElementById("three").style.backgroundColor = colorArray[index];
-          document.getElementById("three").innerHTML = sos[index]+ "<br>"+ text;
-          
-        }
-        if (solutions===3){
-          btn.classList.add("fourth")
-          document.getElementById("four").style.opacity = "1.0";
-          document.getElementById("four").style.backgroundColor = colorArray[index];
-          document.getElementById("four").innerHTML = sos[index]+ "<br>"+ text;
-        }
+  // Mark them as solved
+  matchedButtons.forEach(btn => {
+    btn.classList.remove("active");
 
-        btn.classList.add("Solved");
-    });
+    text=text + btn.textContent+', ';
+    if (solutions===0){
+      btn.classList.add("first")
+      document.getElementById("one").style.opacity = "1.0";
+      document.getElementById("one").style.backgroundColor = colorArray[index];
+      document.getElementById("one").innerHTML = sos[index]+ "<br>"+ text;
+      console.log("punto");
+    }
+    if (solutions===1){
+      btn.classList.add("second") 
+      document.getElementById("two").style.opacity = "1.0";
+      document.getElementById("two").style.backgroundColor = colorArray[index];
+      document.getElementById("two").innerHTML = sos[index]+ "<br>"+ text;
+    }
+    if (solutions===2){
+      btn.classList.add("third") 
+      document.getElementById("three").style.opacity = "1.0";
+      document.getElementById("three").style.backgroundColor = colorArray[index];
+      document.getElementById("three").innerHTML = sos[index]+ "<br>"+ text;
+    }
+    if (solutions===3){
+      btn.classList.add("fourth")
+      document.getElementById("four").style.opacity = "1.0";
+      document.getElementById("four").style.backgroundColor = colorArray[index];
+      document.getElementById("four").innerHTML = sos[index]+ "<br>"+ text;
+    }
 
-    // Buttons that are solved
-    const firstSolved = [...grid.children].filter(btn =>
-        btn.classList.contains("first")
-    );
-    const secondSolved = [...grid.children].filter(btn =>
-        btn.classList.contains("second")
-    );
-    const thirdSolved = [...grid.children].filter(btn =>
-        btn.classList.contains("third")
-    );
-    const fourthSolved = [...grid.children].filter(btn =>
-        btn.classList.contains("fourth")
-    );
-    // All other buttons
-    const unsolvedButtons = [...grid.children].filter(btn =>
-        !btn.classList.contains("Solved")
-    );
+    btn.classList.add("Solved");
+  });
+
+  // Buttons that are solved
+  const firstSolved = [...grid.children].filter(btn =>
+    btn.classList.contains("first")
+  );
+  const secondSolved = [...grid.children].filter(btn =>
+    btn.classList.contains("second")
+  );
+  const thirdSolved = [...grid.children].filter(btn =>
+    btn.classList.contains("third")
+  );
+  const fourthSolved = [...grid.children].filter(btn =>
+    btn.classList.contains("fourth")
+  );
+  // All other buttons
+  const unsolvedButtons = [...grid.children].filter(btn =>
+    !btn.classList.contains("Solved")
+  );
     
   
     
-    // Reorder grid
-    grid.innerHTML = "";
+  // Reorder grid
+  grid.innerHTML = "";
 
-    [...firstSolved, ...secondSolved,...thirdSolved, ...fourthSolved,...unsolvedButtons].forEach(btn => {
-      grid.appendChild(btn);
-    });};
-    function sleep(ms) {
+  [...firstSolved, ...secondSolved,...thirdSolved, ...fourthSolved,...unsolvedButtons].forEach(btn => {
+    grid.appendChild(btn);
+  })
+};
+
+function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
+
 async function solveRemaining() {
-    let k=solutions;
-    for (let i=k; i<4; i++)
-    {
-     const unsolvedButtons = [...grid.children].filter(btn =>
-    !btn.classList.contains("Solved")
-);
+  let k=solutions;
+  for (let i=k; i<4; i++){
+    const unsolvedButtons = [...grid.children].filter(btn =>
+      !btn.classList.contains("Solved")
+    );
 
-if (unsolvedButtons.length === 0) {
-    console.log("No unsolved buttons left");
-    break;
-}
-
-let cleanClass = unsolvedButtons[0].className
-    .replace(/\bactive\b/g, "")
-    .trim();
-
-console.log(cleanClass);
-  const matchedButtons = [...grid.children].filter(btn =>
-    btn.classList.contains(cleanClass));
-
-
-Solve(
-    matchedButtons,
-    cleanClass,
-    categories,
-    document,
-    solutions,
-    colorArray
-);
-
-solutions++;
-await sleep(1500);
+    if (unsolvedButtons.length === 0) {
+      console.log("No unsolved buttons left");
+      break;
     }
+
+    let cleanClass = unsolvedButtons[0].className
+      .replace(/\bactive\b/g, "")
+      .trim();
+
+    console.log(cleanClass);
+    const matchedButtons = [...grid.children].filter(btn =>
+      btn.classList.contains(cleanClass));
+
+
+    Solve(
+      matchedButtons,
+      cleanClass,
+      categories,
+      document,
+      solutions,
+      colorArray
+    );
+
+    solutions++;
+    await sleep(1500);
+  }
 }
 
+function showToast(message, duration = 3000) {
+  const toast = document.getElementById("toast");
+
+  toast.textContent = message;
+  toast.classList.add("show");
+
+  setTimeout(() => {
+    toast.classList.remove("show");
+  }, duration);
+}
 //Word buttons
 
 const colorArray = ["#e2de5d","#7dc27d", "#969dea","#dc75e7"];
 for (let i = 1; i <= 16; i++) {
-    const btn = document.createElement("button");
-    btn.textContent = i;
-    btn.textContent = words[i-1];
-    btn.setAttribute("class", categories[Math.floor((i-1)/4)]);
+  const btn = document.createElement("button");
+  btn.textContent = i;
+  btn.textContent = words[i-1];
+  btn.setAttribute("class", categories[Math.floor((i-1)/4)]);
     
-    btn.addEventListener("click", () => {
-      if(!btn.className.includes("Solved")){
-        if (btn.className.includes("active"))
-          selections--;
+  btn.addEventListener("click", () => {
+    if(!btn.className.includes("Solved")){
+      if (btn.className.includes("active"))
+        selections--;
    
-        if (!btn.className.includes("active"))
-          if(selections<4)
-            selections++;
+      if (!btn.className.includes("active"))
+        if(selections<4)
+          selections++;
 
-        if (max==1)
-          btn.classList.remove("active");
-        else
-          btn.classList.toggle("active");
-        if(selections==4)
-          max=1;
-        else 
-          max=0;
+      if (max==1)
+        btn.classList.remove("active");
+      else
+        btn.classList.toggle("active");
+      if(selections==4)
+        max=1;
+      else 
+        max=0;
     
-        console.log(selections);
-      }
-    });
-    grid.appendChild(btn);
+      console.log(selections);
+    }
+  });
+  grid.appendChild(btn);
 }
+
 function shuffle(){
   // Fisher-Yates shuffle
   const buttons = Array.from(grid.children);
@@ -196,17 +206,19 @@ function shuffle(){
   grid.innerHTML = "";
   buttons.forEach(button => grid.appendChild(button));
 }
+
 //Action buttons
 document.getElementById("shuffleBtn").addEventListener("click", () => {
-    shuffle();
+  shuffle();
 });
+
 document.getElementById("removeBtn").addEventListener("click", () => {
   const buttons = Array.from(grid.children);
   buttons.forEach(btn => btn.classList.remove("active"));
   selections=0;
   max=0;
-    
 });
+
 document.getElementById("submitBtn").addEventListener("click", () => {
   const buttons = Array.from(grid.children);
   var classes = [];
@@ -218,10 +230,10 @@ document.getElementById("submitBtn").addEventListener("click", () => {
   }
   console.log(classes.length);
   
-   if (
+  if (
     classes.length === 4 &&
     classes.every(c => c === classes[0])
-) {
+  ){
     const matchedClass = classes[0];
     console.log(matchedClass);
       
@@ -232,47 +244,35 @@ document.getElementById("submitBtn").addEventListener("click", () => {
     
 
     
-      // Buttons that have the matching class
+    // Buttons that have the matching class
     const matchedButtons = [...grid.children].filter(btn =>
-        btn.classList.contains(cleanClass)
-        
+      btn.classList.contains(cleanClass)
     );
-  console.log(matchedButtons)
+    console.log(matchedButtons)
     Solve(matchedButtons,cleanClass,categories, document, solutions, colorArray);
     selections=0;
     max=0;
     solutions++;
-
-}
+  }
   if (
     classes.length === 4 &&
     !classes.every(c => c === classes[0])
-) {
-  let stuff = "life-"+lifes.toString();
-  lifes--;
-  const exactlyThreeEqual = [...new Set(classes)].some(v => classes.filter(c => c === v).length === 3);
-  if(exactlyThreeEqual)
-    showToast("TRE SU QUATTRO!");
+  ){
+    let stuff = "life-"+lifes.toString();
+    lifes--;
+    const exactlyThreeEqual = [...new Set(classes)].some(v => classes.filter(c => c === v).length === 3);
+    if(exactlyThreeEqual)
+      showToast("TRE SU QUATTRO!");
 
-  console.log("lifes" + lifes);
-  const element = document.getElementById(stuff);
-  element.remove();
-  if (lifes==0){
-    const buttons = Array.from(grid.children);
-    buttons.forEach(btn => btn.classList.remove("active"));
-   solveRemaining();
+    console.log("lifes" + lifes);
+    const element = document.getElementById(stuff);
+    element.remove();
+    if (lifes==0){
+      const buttons = Array.from(grid.children);
+      buttons.forEach(btn => btn.classList.remove("active"));
+    solveRemaining();
+    }
   }
-}
 });
+
 shuffle();
-
-function showToast(message, duration = 3000) {
-  const toast = document.getElementById("toast");
-
-  toast.textContent = message;
-  toast.classList.add("show");
-
-  setTimeout(() => {
-    toast.classList.remove("show");
-  }, duration);
-}
